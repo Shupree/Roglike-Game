103c9c2b86992e8321553ea569db797b923732d1
Shupree <121365360+Shupree@users.noreply.github.com>
2024-12-29 15:13:18 +0900

    메인 메뉴 개발 _01

---
+++ Assets/Scenes/Scripts/UI/MainMenu.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    void Awake()
+    {
+        
+    }
+
+    // 시작 버튼 클릭 시
+    public void ClickStartBtn()
+    {
+        
+    }
+}
